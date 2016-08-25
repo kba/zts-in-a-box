6eba77877181708301b2c2e3927dc28b16a4b931
Philipp Zumstein <zuphilip@gmail.com>
2016-08-25 08:15:10 +0200

    Fix run-win.sh and update docker-compose.yml

--- run-win.sh
+++ run-win.sh
@@ -2,20 +2,16 @@
 
 ## idea is here to make a run script which
 ## makes the necessary substitutes for windows
+## 
+## relative paths are now already handled with ${PWD}
+## sed -e "s@\./@$(pwd)/@g"
+
+sed -e "s/localhost/$(docker-machine ip)/g" docker-compose.yml | docker-compose --file - up "$@"
 
 
+## Create a new docker compose file for win:
 
-## TODO this is not really working...
-
-#DCWIN="$(sed -e 's/localhost/$(docker-machine ip)/g' docker-compose.yml | sed -e 's@\./@$(pwd)/@g')"
-#echo $DCWIN
-#docker-compose -f $DCWIN up "$@"
-
-
-
-## Something which works:
-
-sed -e "s/localhost/$(docker-machine ip)/g" docker-compose.yml | sed -e "s@\./@$(pwd)/@g"
+# sed -e "s/localhost/$(docker-machine ip)/g" docker-compose.yml | sed -e "s@\./@$(pwd)/@g"
 
 ## We can use this to create the necessary docker-compose file with
 ##   ./run-win.sh > dcwin.yml
