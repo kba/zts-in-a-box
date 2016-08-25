fea519a207317ff37adbb0baa1b97af3623f01af
Konstantin Baierer <unixprog@gmail.com>
2016-08-25 09:17:35 +0200

    Fix typos in update-host.sh script

--- script/update-host.sh
+++ script/update-host.sh
@@ -5,8 +5,8 @@ set -e
 logerr ()  { echo -e "[\e[1;31merror\e[0m] $*" >&2; }
 loginfo ()  { echo -e "[\e[1;32minfo\e[0m] $*" >&2; }
 
-DOCKER_MACHINE=${DOCKER_MACHINE:$1}
-DOCKER_MACHINE=${DOCKER_MACHINE:default}
+DOCKER_MACHINE=${DOCKER_MACHINE:-$1}
+DOCKER_MACHINE=${DOCKER_MACHINE:-default}
 
 if [[ "$(uname -s)" = 'Linux' ]];then
   logerr "This script should only be run in Windows or Mac OSX"
@@ -22,6 +22,6 @@ ZTS_HOST="$(docker-machine ip "$DOCKER_MACHINE"):${ZTS_PORT}"
 
 loginfo "Setting ZTS_HOST to '$ZTS_HOST'"
 
-sed -i "s,^ZTS_HOST=.*,ZTS_PORT=$ZTS_HOST," .env
+sed -i "s,^ZTS_HOST=.*,ZTS_HOST=$ZTS_HOST," .env
 
 loginfo "Application will be available at http://$ZTS_HOST"
